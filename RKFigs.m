bf3b63d0208cbaa344da8ba0e7eec5a8e309fe5c
Fred J Hickernell <hickernell@iit.edu>
2021-11-15 00:32:18 -0600

    Office Computer Commits

--- RKFigs.m
+++ RKFigs.m
@@ -1,21 +1,29 @@
 %% Plot RK figs
 gail.InitializeWorkspaceDisplay
 
-absdif = @(t,x) (reshape(t,[size(t,1),1,size(t,2)]) - ...
+differ = @(t,x) (reshape(t,[size(t,1),1,size(t,2)]) - ...
     reshape(x,[1,size(x,1),size(x,2)]));
 
-sqexpker = @(t,x,c) squeeze(exp(-sum((absdif(t,x) .* reshape(c,[1,1,size(c,2)])).^2,3)));
+normsq = @(t,x,c) squeeze(sum((differ(t,x) .* reshape(c,[1,1,size(c,2)])).^2,3)); 
+
+normof = @(t,x,c) sqrt(normsq(t,x,c));
+
+sqexpker = @(t,x,c) exp(-normsq(t,x,c));
 
 
 ctrdiscker = @(t,x,c) squeeze(prod(1 + (abs(reshape(t,[size(t,1),1,size(t,2)])-1/2) +...
     abs(reshape(x,[1,size(x,1),size(x,2)])-1/2) - ...
-    absdif(t,x)) .* reshape(c,[1,1,size(c,2)]),3));
+    differ(t,x)) .* reshape(c,[1,1,size(c,2)]),3));
+
+maternkerthreehalfs = @(t,x,c) (1 + normof(t,x,c)).*exp(-normof(t,x,c));
+
+deltaker = @(t,x,c) 1 + c(1).*(differ(t,x)==0);
 
 
-kernelEx = {sqexpker,ctrdiscker};
-kernelName = {'sqexpker','ctrdiscker'};
-cVal = [0.5 2 4]';
-c2Val = [4 2];
+kernelEx = {sqexpker,ctrdiscker,maternkerthreehalfs};
+kernelName = {'sqexpker','ctrdiscker','maternkerthreehalfs'};
+cVal = [0.5 2 8]';
+c2Val = [8 2];
 tc = 0.5;
 tc2 = [0.5 0.5];
 xplot = (0:0.002:1)';
@@ -32,10 +40,15 @@ for jj = 1:length(kernelEx)
     for ii = 1:length(cVal)
         kplot(:,ii) = kernel(xplot,tc,cVal(ii));
     end
+    kplot(isnan(kplot)) = 1;
     h = plot(xplot,kplot);
     xlabel('\(x\)')
-    ylabel(['\(K(x,' num2str(tc) ')\)'])
-    legendCell = cellstr(num2str(cVal, 'c = %-g'));
+    if jj < 3
+        ylabel(['\(K(x,' num2str(tc) ')\)'])
+    else
+        ylabel(['\(K_{3/2}(x,' num2str(tc) ')\)'])
+    end
+    legendCell = cellstr(num2str(cVal, '\\(\\gamma = %-g\\)'));
     legend(h,legendCell,'Box','off','location','best')
     print('-depsc',['RK-' kernelName{jj} '.eps'])
 
@@ -44,9 +57,21 @@ for jj = 1:length(kernelEx)
     kk = reshape(kplot2,size(xx,1),size(xx,1));
     h = surf(xxplot2,yyplot2,kk,'FaceColor','interp','EdgeColor','interp');
     xlabel('\(x_1\)')
-    ylabel('\(x_1\)')
+    ylabel('\(x_2\)')
     zlabel(['\(K\bigl((x_1,x_2),(' num2str(tc2(1)) ',' num2str(tc2(1)) ') \bigr)\)'])
+    title(['\(\gamma_1 = ' num2str(c2Val(1)) ', \gamma_2 = ' num2str(c2Val(2)) '\)' ])
     print('-depsc',['RK2-' kernelName{jj} '.eps'])
-    title(['\(c_1 = ' num2str(c2Val(1)) ', c_2 = ' num2str(c2Val(2)) '\)' ])
 
 end
+
+%% For delta kernel
+figure
+kplot2 = 1 + (xplot2(:,1) == xplot2(:,2));
+kk = reshape(kplot2,size(xx,1),size(xx,1));
+h = surf(xxplot2,yyplot2,kk,'FaceColor','interp','EdgeColor','interp');
+xlabel('\(x_1\)')
+ylabel('\(x_2\)')
+zlabel(['\(K(x_1,x_2)\)'])
+title('\(\gamma = 1\)')
+print('-depsc','RK-deltaker.eps')
+
