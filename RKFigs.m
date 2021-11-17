97dcf631df5a2355917be6d9b3c52c159e378bd0
Fred J Hickernell <hickernell@iit.edu>
2021-11-17 15:33:59 -0600

    Office Computer Commits

--- RKFigs.m
+++ RKFigs.m
@@ -11,9 +11,9 @@ normof = @(t,x,c) sqrt(normsq(t,x,c));
 sqexpker = @(t,x,c) exp(-normsq(t,x,c));
 
 
-ctrdiscker = @(t,x,c) squeeze(prod(1 + (abs(reshape(t,[size(t,1),1,size(t,2)])-1/2) +...
+ctrdiscker = @(t,x,c) squeeze(prod(1 + (1/2)*(abs(reshape(t,[size(t,1),1,size(t,2)])-1/2) +...
     abs(reshape(x,[1,size(x,1),size(x,2)])-1/2) - ...
-    differ(t,x)) .* reshape(c,[1,1,size(c,2)]),3));
+    abs(differ(t,x))) .* reshape(c,[1,1,size(c,2)]),3) );
 
 maternkerthreehalfs = @(t,x,c) (1 + normof(t,x,c)).*exp(-normof(t,x,c));
 
@@ -22,11 +22,16 @@ deltaker = @(t,x,c) 1 + c(1).*(differ(t,x)==0);
 
 kernelEx = {sqexpker,ctrdiscker,maternkerthreehalfs};
 kernelName = {'sqexpker','ctrdiscker','maternkerthreehalfs'};
+kertitles = {'Squared Exponential Kernel','Centered Discrepancy Kernel', ...
+    'Matern Three-Halves Kernel'};
 cVal = [0.5 2 8]';
 c2Val = [8 2];
-tc = 0.5;
-tc2 = [0.5 0.5];
+tc = 0.4;
+tc2 = [0.4 0.6];
 xplot = (0:0.002:1)';
+xdata = (0:0.1:1)';
+xcard = 4;
+ecard = zeros(size(xdata)); ecard(xcard) = 1;
 xx = (0:0.01:1)';
 [xxplot2, yyplot2] = meshgrid(xx);
 xplot2 = [xxplot2(:) yyplot2(:)];
@@ -35,7 +40,7 @@ kplot2(size(xplot2,1),1) = 0;
 legend
 for jj = 1:length(kernelEx)
     kernel = kernelEx{jj};
-    figure(2*jj-1);
+    figure(3*jj-2);
     hold on
     for ii = 1:length(cVal)
         kplot(:,ii) = kernel(xplot,tc,cVal(ii));
@@ -52,13 +57,23 @@ for jj = 1:length(kernelEx)
     legend(h,legendCell,'Box','off','location','best')
     print('-depsc',['RK-' kernelName{jj} '.eps'])
 
-    figure(2*jj);
+    figure(3*jj-1)
+    kx = kernel(xplot,xdata,cVal(end));
+    kxx = kernel(xdata,xdata,cVal(end));
+    card = kx*(kxx\ecard);
+    plot(xplot,card,'-',xdata,ecard,'.')
+    xlabel('\(x\)')
+    ylabel('Cardinal Function')
+    title(kertitles{jj})
+    print('-depsc',['Card-' kernelName{jj} '.eps'])
+
+    figure(3*jj);
     kplot2(:) = kernel(xplot2,tc2,c2Val);
     kk = reshape(kplot2,size(xx,1),size(xx,1));
     h = surf(xxplot2,yyplot2,kk,'FaceColor','interp','EdgeColor','interp');
     xlabel('\(x_1\)')
     ylabel('\(x_2\)')
-    zlabel(['\(K\bigl((x_1,x_2),(' num2str(tc2(1)) ',' num2str(tc2(1)) ') \bigr)\)'])
+    zlabel(['\(K\bigl((x_1,x_2),(' num2str(tc2(1)) ',' num2str(tc2(2)) ') \bigr)\)'])
     title(['\(\gamma_1 = ' num2str(c2Val(1)) ', \gamma_2 = ' num2str(c2Val(2)) '\)' ])
     print('-depsc',['RK2-' kernelName{jj} '.eps'])
 
